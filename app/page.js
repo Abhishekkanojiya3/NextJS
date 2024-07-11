24a848abb2a47b7811ad6563c7b6a8ce2d899e50
Abhishekkanojiya3 <abhi67562@gmail.com>
2024-07-11 16:13:43 +0530

    Authentication

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
-import ContactForm from "@/components/ContactForm";
 
 export default function page() {
   return (
     <div>
-      <h1>Contact Form</h1>
-      <ContactForm />
+      <h1>Auth</h1>
     </div>
   )
 }
